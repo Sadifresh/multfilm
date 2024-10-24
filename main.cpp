ecd9d80fb3d9b309c7e89b173c19cf9f362f0fa9
Sadifresh <sadifresh.228@gmail.com>
2024-10-24 09:29:49 +0300

    1c

--- main.cpp
+++ main.cpp
@@ -2,32 +2,32 @@
 
 void drawFon()
 {
-
-}
-
-
-int main()
-{
-txCreateWindow (800, 600);
-
-
-
-    //фон
     txSetColor (TX_BLUE);
     txSetFillColor (TX_BLUE);
     txRectangle(0,0,800,600);
+}
 
-    //солнце
+void drawSun(int x)
+{
     txSetColor (TX_YELLOW);
     txSetFillColor (TX_YELLOW);
-    txCircle(275,120,50);
+    txCircle(x,120,50);
+}
 
-    //облако
+void drawCloud(int x)
+{
+//x=455
     txSetColor (TX_WHITE);
     txSetFillColor (TX_WHITE);
-    txEllipse(505,95,690,145);
+    txEllipse(x+505-455,95,x+690-455,145);
+    txEllipse(x+455-455,100,x+560-455,140);
+    txEllipse(x+625-455,95,x+730-455,140);
+    txEllipse(x+510-455,70,x+670-455,120);
 
-    //дом
+}
+
+void drawHouse()
+{
     txSetColor (TX_WHITE);
     txSetFillColor (TX_YELLOW);
     txRectangle (210, 220+170, 380, 390+170);
@@ -39,6 +39,43 @@ txCreateWindow (800, 600);
     txPolygon (kr, 3);
     POINT skat[4] = {{300, 120+170}, {450, 60+170}, {515 ,130+170}, {380,220+170}};
     txPolygon (skat, 4);
+}
+
+int main()
+{
+txCreateWindow (800, 600);
+
+    int xSun=-100;
+    int xCloud=1000;
+
+
+    while(xSun<900)
+    {
+        txBegin();
+        drawFon();
+        drawSun(xSun);
+        drawCloud(xCloud);
+        drawHouse();
+        txEnd();
+
+        xSun += 2;
+        xCloud -= 2;
+        txSleep(10);
+    }
+
+    while(xCloud>-500)
+    {
+        txBegin();
+        drawFon();
+        drawSun(xSun);
+        drawCloud(xCloud);
+        drawHouse();
+        txEnd();
+
+
+        xCloud -= 2;
+        txSleep(10);
+    }
 
 
 
